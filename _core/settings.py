23bd1cbfcbb000da737c42d3df312e4c3388374d
felipemotavieira <felipemtvieira@gmail.com>
2023-02-12 18:15:55 -0300

    railway deploy settings

--- _core/settings.py
+++ _core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://web-production-5fb3.up.railway.app/', '0.0.0.0']
 
 RENDER_EXTERNAL_HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
